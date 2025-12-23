f08c1577e5b992ad57b9b61bb902176e478889b1
Dhruv <dhruvsahu7355@gmail.com>
2025-12-23 11:17:48 +0530

    [Dhruv] Add. Added the solution of extra string handling Problems

---
+++ core-java-practice/gcr-codebase/string/extraStringProblems/ToggleCase.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+class ToggleCase {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        String toggled = toggleCase(input);
+
+        System.out.println("Toggled string: " + toggled);
+    }
+
+    public static String toggleCase(String str) {
+        StringBuilder result = new StringBuilder();
+
+        for (char ch : str.toCharArray()) {
+            if (Character.isUpperCase(ch)) {
+                result.append(Character.toLowerCase(ch));
+            } else if (Character.isLowerCase(ch)) {
+                result.append(Character.toUpperCase(ch));
+            } else {
+                result.append(ch); 
+            }
+        }
+
+        return result.toString();
+    }
+}
