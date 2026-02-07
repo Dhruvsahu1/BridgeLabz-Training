407bec23f2d092d6b38e0d2f9456d86a79962831
Dhruv <dhruvsahu7355@gmail.com>
2026-02-07 10:42:02 +0530

    [Dhruv] Add. added the solution of Sonar solution in junit project

---
+++ io-programming-practice/gcr-codebase/sonar/Dhruvsha/src/main/java/Dhruv/Dhruvsha/App.java
@@ -0,0 +1,13 @@
+package Dhruv.Dhruvsha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
